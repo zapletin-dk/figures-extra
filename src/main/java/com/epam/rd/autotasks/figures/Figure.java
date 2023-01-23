3bb4adb56ae4f20f6908dbee948741e4eef149e4
Zapletin Daniil <Zaplot1994@gmail.com>
2023-01-23 19:35:07 +0300

    Task done with all tests passed

--- src/main/java/com/epam/rd/autotasks/figures/Figure.java
+++ src/main/java/com/epam/rd/autotasks/figures/Figure.java
@@ -1,6 +1,7 @@
 package com.epam.rd.autotasks.figures;
 
 abstract class Figure{
+    public static final double ERROR_DELTA = 1e-10;
 
     public abstract Point centroid();
     public abstract boolean isTheSame(Figure figure);
